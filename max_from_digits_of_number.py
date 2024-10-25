c90498de8f4ca24236bbc1c53ec30ecacfa04024
CiprianRad <89615038+CiprianRad@users.noreply.github.com>
2024-10-25 12:23:04 +0300

    max_from_digits_of_number.py

--- max_from_digits_of_number.py
+++ max_from_digits_of_number.py
@@ -9,15 +9,15 @@ def get_valid_input():
         except ValueError:
             print("Please enter a valid integer: ")
 
+
 def max_number(n):
-    list_from_number = list(str(n))
-    list_from_number.sort(reverse = True)
-    list_from_number = ''.join(list_from_number)
+    list_from_number = list(str(n))  # We declare a list with string values, each entry is the digit of a given number
+    list_from_number.sort(reverse=True)  # We sort the list alphanumerically in descending order
+    list_from_number = ''.join(list_from_number)  # We join the elements in the list together. They form the number
     return list_from_number
 
 
 def main():
-
 
     n = get_valid_input()
     print("The greatest number that can be made using the digits of your number is: ", max_number(n))
